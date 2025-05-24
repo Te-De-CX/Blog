36faea59fb4a369f1664bdd704d9d3dc2a150fdd
teslim <tunjiheritage@gmail.com>
2025-05-24 19:59:02 +0100

    main

--- blog-post/src/components/UI/NavBar/Nav.tsx
+++ blog-post/src/components/UI/NavBar/Nav.tsx
@@ -1,5 +1,7 @@
 
 import Link from "next/link";
+import Image from "next/image";
+import WebsiteLogo from "../../../../public/images/logo/logo.jpg"
 
 const Nav = () => {
 
@@ -12,29 +14,35 @@ const Nav = () => {
 
     return (
         <>
-            <nav>
-                <div>
+            <nav className="flex flex-col" >
+                <div className="text-sm text-center py-5 bg-gray-900" >
                     subscribe to our newsletter for new & latest blogs and resources
                 </div>
-                <div>
-                    <div>
-                        logo
+                <div className="flex px-10 py-5 justify-between bg-amber-900 items-center" >
+                    <div className="flex" >
+                        <div>
+                            <Image
+                            src={WebsiteLogo}
+                            alt="websites logo"
+                            className="w-10"
+                            />
+                        </div>
+                        <p>futureTech</p>
                     </div>
-                    <p>futureTech</p>
-                </div>
-                <div>
-                    {
-                        pages.map( value => (
-                            <Link href={value.link} key={value.id}>
-                                {value.name}
-                            </Link>
-                        ) )
-                    }
-                </div>
-                <div>
-                    <Link href={"/contact"} >
-                        contact us
-                    </Link>
+                    <div className="flex gap-10" >
+                        {
+                            pages.map( value => (
+                                <Link href={value.link} key={value.id} className="capitalize bg-gray-600 border-2 border-white py-3 px-5 rounded-xl" >
+                                    {value.name}
+                                </Link>
+                            ) )
+                        }
+                    </div>
+                    <div>
+                        <Link href={"/contact"} >
+                            contact us
+                        </Link>
+                    </div>
                 </div>
             </nav>
         </>
